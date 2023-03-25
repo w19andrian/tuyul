c9eb9713a18c4bddf7c65a48c558d1b9cb632827
Wemphi Andrian <wempiandrian@gmail.com>
2023-03-25 03:23:21 +0100

    change cert's common name and alternative name

--- .infra/terraform/tuyul-app/certs.tf
+++ .infra/terraform/tuyul-app/certs.tf
@@ -1,3 +1,6 @@
+locals {
+  tld_common_name = var.env != "production" ? "*.${local.env_alias}.${data.cloudflare_zone.this.name}" : data.cloudflare_zone.this.name
+}
 resource "tls_private_key" "private_key" {
   algorithm = "RSA"
 }
@@ -9,8 +12,8 @@ resource "acme_registration" "registration" {
 
 resource "acme_certificate" "certificate" {
   account_key_pem           = acme_registration.registration.account_key_pem
-  common_name               = data.cloudflare_zone.this.name
-  subject_alternative_names = ["*.${data.cloudflare_zone.this.name}"]
+  common_name               = local.tld_common_name
+  subject_alternative_names = [local.tld_common_name]
 
   dns_challenge {
     provider = "cloudflare"
